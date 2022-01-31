522cced3419605da6c73863c7972605c6fa89e1d
Ramya Ramamurthy <ramya.rr@gmail.com>
2022-01-31 08:02:15 -0800

    Adding mapping_points_and_lines folder and files

---
+++ mapping_lines_and_points/mapping_lines/static/js/logic.js
@@ -0,0 +1,45 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.7749, -122.4194], 4);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Coordinates for each point to be used in the polyline.
+// showing the route from LAX, SFO, SLC, and SEA:
+let line = [
+    [37.7749, -122.4194],
+    [30.1975, -97.6664],
+    [43.6777, 79.6248],
+    [40.6413, -73.7781]
+  ];
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "black",
+        fillColor: "#ffffa1"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: 'blue', weight: '2',  dashArray: '20, 20', dashOffset: '0'
+  }).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
